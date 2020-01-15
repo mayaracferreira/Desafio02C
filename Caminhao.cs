16bfad589b67efd64702cf5c328299998a332aa8
Mayara Ferreira <mayara.c.ferreira@hotmail.com>
2020-01-15 14:23:30 -0300

    Desafio02-Herança

---
+++ Caminhao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio02___Herança
+{
+    class Caminhao
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio02___Herança
+{
+    class Program
+    {
+   
+        
+            static void Main(string[] args)
+            {
+
+
+                Carro c1 = new Carro("honda", "city", "FAM4334", "cinza", 10, true, 50, 10, 38000, 4, 2016);
+
+                Aviao a1 = new Aviao("booing", "773", "5555", "branco", 10, true, 50, 100, 80000, "comercial", "corporativo");
+
+
+
+                c1.status();
+            Console.WriteLine("---------------------------------------");
+                a1.status();
+
+
+
+
+
+
+
+            }
+        
+    }
+}
+
